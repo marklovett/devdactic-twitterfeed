2a5fa3b4e5d376f6a168653c82185843d2634714
Mark Lovett <mark@marklovett.com>
2015-05-28 21:55:12 -0400

    initial commit

---
+++ www/js/app.js
@@ -0,0 +1,17 @@
+// http://blog.ionic.io/displaying-the-twitter-feed-within-your-ionic-app/
+
+
+angular.module('starter', ['ionic','ngResource', 'ngCordova'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
